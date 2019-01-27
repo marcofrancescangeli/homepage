eb9fcb5cbc175339ec764be2139f734951be676d
Marco Francescangeli <marco@francescange.li>
2019-01-27 19:56:49 +0000

    Updates

---
+++ precache-manifest.e05ee698bdee66a1201ae2cc88be6079.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "27102b6f129493b553f0",
+    "url": "/homepage/./static/css/main.e9fb7a76.chunk.css"
+  },
+  {
+    "revision": "27102b6f129493b553f0",
+    "url": "/homepage/./static/js/main.27102b6f.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/homepage/./static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "9a1ebfdf49b23476bb3a",
+    "url": "/homepage/./static/js/runtime~main.9a1ebfdf.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/homepage/./static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "fe26da2481c846a8f6792aa7e3a6d54f",
+    "url": "/homepage/./index.html"
+  }
+];
